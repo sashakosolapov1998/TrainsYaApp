81eeb77c0b14010bf16a7918b5cf7adc75df75e6
Александр Косолапов <sashakosolapov1998@yandex.ru>
2025-08-21 14:40:45 +0300

    first commit

---
+++ TrainsYa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrainsYa
+//
+//  Created by Александр Косолапов on 21/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrainsYa/TrainsYaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainsYaApp.swift
+//  TrainsYa
+//
+//  Created by Александр Косолапов on 21/8/25.
+//
+
+import SwiftUI
+
+@main
+struct TrainsYaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
